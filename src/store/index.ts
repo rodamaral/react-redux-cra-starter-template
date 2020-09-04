34d773d5b04d3c2ce0efa964a027673b05ab76e3
Rodrigo A. do Amaral <rodrigo.amaral@ipam.org.br>
2020-09-04 12:50:38 -0300

    Setup redux-toolkit

---
+++ src/store/index.ts
@@ -0,0 +1,28 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { createLogger } from 'redux-logger'
+import { combineReducers } from 'redux'
+// import auth from './auth'
+
+const rootReducer = combineReducers({
+    // auth,
+})
+
+const loggerMiddleware = createLogger()
+
+export default function configureAppStore(preloadedState: any) {
+    const store = configureStore({
+        reducer: rootReducer,
+        // redux-logger must be the last middleware so as to not log functions/promises
+        middleware: [...getDefaultMiddleware(), loggerMiddleware],
+        preloadedState,
+    })
+
+    // TODO: hot module replacement
+    if (process.env.NODE_ENV === 'development' && module.hot) {
+        module.hot?.accept('./', () => {
+            module.hot?.accept('./', () => store.replaceReducer(rootReducer))
+        })
+    }
+
+    return store
+}
